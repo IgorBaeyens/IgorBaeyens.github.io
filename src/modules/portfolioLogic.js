a77877804be0432feb04f1c64a6323f94822782e
IgorBaeyens <56031129+IgorBaeyens@users.noreply.github.com>
2023-09-12 21:08:29 +0200

    image viewer update

--- src/modules/portfolioLogic.js
+++ src/modules/portfolioLogic.js
@@ -2,10 +2,11 @@
 
 import gsap from "gsap"
 
-
 let portfolio = document.getElementsByClassName("portfolio__items")[0]
+let imageViewer = document.getElementsByClassName("portfolio-image-viewer")[0]
+let imageViewerClose = document.getElementsByClassName("portfolio-image-viewer__close-button")[0]
+let imageViewerInfo = document.getElementsByClassName("portfolio-image-viewer__info")[0]
 let carouselSlider = document.getElementsByClassName("carousel__slider")[0]
-let itemViewerInfo = document.getElementsByClassName("portfolio-image-viewer__info")[0]
 let carouselDots = document.getElementsByClassName("image-viewer__dots")[0]
 let portfolioItems
 
@@ -35,11 +36,11 @@ const addPortfolioLogic = () => {
         }
         portfolio.innerHTML = htmlPortfolioItems
 
-        getPortfolioItems(portfolioData)
+        addImageViewer(portfolioData)
 
     }
 
-    const getPortfolioItems = (portfolioData) => {
+    const addImageViewer = (portfolioData) => {
         portfolioItems = document.getElementsByClassName("portfolio__item")
         
         for (const portfolioItemElem of portfolioItems) {
@@ -47,17 +48,36 @@ const addPortfolioLogic = () => {
                 let itemElementTitle = event.currentTarget.getElementsByClassName("portfolio__item__title")[0].textContent
                 for (const portfolioItem of portfolioData) {
                     if (itemElementTitle == portfolioItem.title) {
+                        imageViewerAnimation.play()
+
                         addCarouselImages(portfolioItem)
                         
                         addItemViewerInfo(portfolioItem)
                         
                         addCarouselLogic()
+
+                        // imageViewer.style.display = "block"
+                        
                     }
                 }
                 
             })
         }
+
+       imageViewerClose.addEventListener('click', () => {
+            // portfolio.style.display = "grid"
+            
+            imageViewerAnimation.reverse()
+       })
     }
+
+    // TODO add animation
+    let imageViewerAnimation = gsap.timeline()
+    imageViewerAnimation.to(portfolio, {opacity: 0, duration: 0.2, ease: "Power1.easeInOut"})
+    imageViewerAnimation.to(portfolio, {display: "none", duration: 0})
+    imageViewerAnimation.to(imageViewer, {display: "block", duration: 0})
+    imageViewerAnimation.fromTo(imageViewer, {opacity: 0, translateY: 100, duration: 0.8, ease: "Power1.easeInOut"}, {opacity: 1, translateY: 0})
+    imageViewerAnimation.paused(true)
 
     const addCarouselImages = (portfolioItem) => {
         let htmlItemSlides = ``
@@ -82,15 +102,17 @@ const addPortfolioLogic = () => {
         let carouselDotElems = document.getElementsByClassName("image-viewer__dot")
         let carouselSlide = document.getElementsByClassName("carousel__slide")[0]
         for (const dotElem of carouselDotElems) {
+            if (dotElem.value == "image-1") {
+                dotElem.checked = true
+                gsap.to(carouselSlider, {translateX: 0, duration: 0.05})
+            }
             dotElem.addEventListener('change', (event) => {
                 let imageWidth = parseInt(window.getComputedStyle(carouselSlide).width) 
 
-                console.log(event.target.value)
                 if (event.target.value == "image-1") {
                     gsap.to(carouselSlider, {translateX: 0})
                 } else {
-                    gsap.to(carouselSlider, {translateX: -imageWidth})
-
+                    gsap.to(carouselSlider, {translateX: -imageWidth * (dotElem.value.replace(/\D/g,'') - 1)})
                 }
             })
         }
@@ -102,7 +124,7 @@ const addPortfolioLogic = () => {
             <p class="portfolio-image-viewer__link">${portfolioItem.twitter.name}</p>
             <p class="portfolio-image-viewer__date">${portfolioItem.date}</p>
         `
-        itemViewerInfo.innerHTML = htmlItemViewerInfo 
+        imageViewerInfo.innerHTML = htmlItemViewerInfo 
     }
     
 }
